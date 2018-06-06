d79fdd9b5cac8d5cdf7f4a3af88c3536471857b9
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-06-06 15:26:21 +0200

    chore: setup markup

--- React InstantSearch/saas/src/App.js
+++ React InstantSearch/saas/src/App.js
@@ -1,51 +1,31 @@
 import React, { Component } from 'react';
-import {
-  InstantSearch,
-  Hits,
-  SearchBox,
-  Pagination,
-} from 'react-instantsearch/dom';
+import { InstantSearch } from 'react-instantsearch/dom';
 import PropTypes from 'prop-types';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <header className="header">
-          <h1 className="header-title">
-            <a href="/">saas</a>
-          </h1>
-          <p className="header-subtitle">
-            using{' '}
-            <a href="https://github.com/algolia/react-instantsearch">
-              React InstantSearch
-            </a>
-          </p>
-        </header>
-
-        <div className="container">
-          <InstantSearch
-            appId="B1G2GM9NG0"
-            apiKey="aadef574be1f9252bb48d4ea09b5cfe5"
-            indexName="demo_saas"
-          >
-            <div className="container-app">
-              <div className="filters">
-              </div>
-
-              <div className="search-container">
-                <SearchBox className="searchBox" placeholder="" />
-                <Hits hitComponent={Hit} />
-
-                <div className="pagination">
-                  <Pagination />
-                </div>
-              </div>
-            </div>
-          </InstantSearch>
-        </div>
-      </div>
+      <InstantSearch
+        appId="B1G2GM9NG0"
+        apiKey="aadef574be1f9252bb48d4ea09b5cfe5"
+        indexName="demo_saas"
+      >
+        <main className="search-container">
+          <div className="left-panel">
+            <div id="type">RefinementList</div>
+          </div>
+          <div className="right-panel">
+            <div id="searchbox">SearchBox</div>
+            <div id="stats">Stats</div>
+            <div id="hits">Hits</div>
+          </div>
+        </main>
+        <footer>
+          Source Code on <a href="#">GitHub</a> - Powered by{' '}
+          <a href="https://www.algolia.com">Algolia</a>
+        </footer>
+      </InstantSearch>
     );
   }
 }
